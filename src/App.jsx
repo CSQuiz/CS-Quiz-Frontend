77631a3e6accb633d50205010869e36a01d4a4fa
yunadada <ynson0216@naver.com>
2025-02-21 15:02:03 +0900

    feat: 페이지 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/home";
+import QuizBox from "./components/QuizBox/quizBox";
+import Score from "./components/Score/score";
+import Lank from "./components/Lank/lank";
 
 function App() {
   return (
     <>
       <h1>CS Quiz</h1>
-      <div />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/quiz" element={<QuizBox />} />
+          <Route path="/score" element={<Score />} />
+          <Route path="/lank" element={<Lank />} />
+        </Routes>
+      </Router>
     </>
   );
 }
