071755b5520e98f5eb2ef9cca455a1b479f95e90
yunadada <ynson0216@naver.com>
2025-03-02 21:28:48 +0900

    feat: 전체 순위 화면 기능 구현

--- src/components/Lank/lank.jsx
+++ src/components/Lank/lank.jsx
@@ -1,18 +1,33 @@
+import { useEffect, useState } from "react";
 import User from "../User/user";
 import style from "./lank.module.css";
+import axios from "axios";
 
 const Lank = () => {
+  const [rank, setRank] = useState([]);
+
+  useEffect(() => {
+    requestRanking();
+  }, []);
+
+  const requestRanking = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:8080/api/users/ranking"
+      );
+      setRank(response);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div className={style.container}>
       <div className={style.title}>순위</div>
       <div className={style.contents}>
-        <User />
-        <User />
-        <User />
-        <User />
-        <User />
-        <User />
-        <User />
+        {rank.map((user, index) => {
+          <User key={index} user={user} lank={index} />;
+        })}
       </div>
     </div>
   );
