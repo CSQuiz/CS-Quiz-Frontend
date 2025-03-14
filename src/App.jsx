d752f7868f7b4d6185e5fa2e8d3181b1e9a23cd5
yunadada <ynson0216@naver.com>
2025-03-14 23:11:47 +0900

    fix: 모달 상태 관리 추가 및 모달 상태에 따른 조건부 렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,33 @@ import Home from "./components/Home/home";
 import QuizBox from "./components/QuizBox/quizBox";
 import Score from "./components/Score/score";
 import Rank from "./components/Rank/rank";
+import CustomModal from "./components/CustomModal/customModal";
+import { useState } from "react";
 
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalText, setModalText] = useState("");
+
   return (
     <>
-      <h1>CS Quiz</h1>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/quiz" element={<QuizBox />} />
-          <Route path="/score" element={<Score />} />
-          <Route path="/rank" element={<Rank />} />
-        </Routes>
-      </Router>
+      {modalOpen && <CustomModal handleModal={setModalOpen} text={modalText} />}
+      <div className="wrapper">
+        <h1>CS Quiz</h1>
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Home setModalOpen={setModalOpen} setModalText={setModalText} />
+              }
+            />
+            <Route path="/quiz" element={<QuizBox />} />
+            <Route path="/score" element={<Score />} />
+            <Route path="/rank" element={<Rank />} />
+            <Route path="/modal" element={<CustomModal />} />
+          </Routes>
+        </Router>
+      </div>
     </>
   );
 }
